a3e5af9d58969bcbe089dafb4a3067a43147fe77
Jhon Useche <jhon.useche@telus.com>
2022-12-05 16:56:32 -0700

    Add code to change the color of the counter text to red if there are more than 140 characters.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,12 @@ $(document).ready(function(e) {
   let maxLength = 140;
   $("textarea").on("keyup", function() {
     let tweetLength = maxLength - $(this).val().length;
+    if (tweetLength < 0) {
+      $(".counter").addClass("counter-negative");
+    }
+    if (tweetLength >= 0) {
+      $(".counter").removeClass("counter-negative");
+    }
     $(".counter").text(tweetLength);
   });
 });
