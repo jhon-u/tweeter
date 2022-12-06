fc37e0d0e23586456e9e4dc6d309950f41939ff1
Jhon Useche <jhon.useche@telus.com>
2022-12-06 13:11:54 -0700

    Remove the defer attribute from the app js script tags and used jquery document ready method instead.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -29,59 +29,64 @@ const data = [
   }
 ];
 
-const renderTweets = function(tweets) {
-  // loops through tweets
-  for (const index in tweets) {
-    const tweet = {
-      username: tweets[index].user.name,
-      avatar: tweets[index].user.avatars,
-      handle: tweets[index].user.handle,
-      text: tweets[index].content.text,
-      createdAt: tweets[index].created_at
-    };
-    console.log(tweet);
-    // calls createTweetElement for each tweet
-    const tweetElement = createTweetElement(tweet);
-    // takes return value and appends it to the tweets container
-    $("#tweets-container").append(tweetElement);
-  }
-};
+$(document).ready(function() {
 
-const createTweetElement = function(tweet) {
-  let $tweet = `
-  <article class="tweet">
-  <header>
-  <div class="left-box">
-  <img src="${tweet.avatar}">
-  <div class="tweet-name">
-  ${tweet.username}
-  </div>
-  </div>
-  <div class="handle">
-  ${tweet.handle}
-  </div>
-  </header>
-  <main>
-  <div class="tweet-text">
-  ${tweet.text}
-  </div>
-  <div class="tweet-divider"></div>
-  </main>
-  <footer>
-  <div>
-  ${tweet.createdAt}
-  </div>
-  <div class="tweet-options">
-  <i class="fa-solid fa-flag"></i>
-  <i class="fa-solid fa-retweet"></i>
-  <i class="fa-solid fa-heart"></i>
-  </div>
-        </footer>
-        </article>
-        `;/* Your code for creating the tweet element */
-  // ...
-  return $tweet;
-};
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    for (const index in tweets) {
+      const tweet = {
+        username: tweets[index].user.name,
+        avatar: tweets[index].user.avatars,
+        handle: tweets[index].user.handle,
+        text: tweets[index].content.text,
+        createdAt: tweets[index].created_at
+      };
+      console.log(tweet);
+      // calls createTweetElement for each tweet
+      const tweetElement = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $("#tweets-container").append(tweetElement);
+    }
+  };
+  
+  const createTweetElement = function(tweet) {
+    let $tweet = `
+    <article class="tweet">
+    <header>
+    <div class="left-box">
+    <img src="${tweet.avatar}">
+    <div class="tweet-name">
+    ${tweet.username}
+    </div>
+    </div>
+    <div class="handle">
+    ${tweet.handle}
+    </div>
+    </header>
+    <main>
+    <div class="tweet-text">
+    ${tweet.text}
+    </div>
+    <div class="tweet-divider"></div>
+    </main>
+    <footer>
+    <div>
+    ${tweet.createdAt}
+    </div>
+    <div class="tweet-options">
+    <i class="fa-solid fa-flag"></i>
+    <i class="fa-solid fa-retweet"></i>
+    <i class="fa-solid fa-heart"></i>
+    </div>
+          </footer>
+          </article>
+          `;/* Your code for creating the tweet element */
+    // ...
+    return $tweet;
+  };
+
+  renderTweets(data);
+});
 
 // Add comments here
 $("document").ready(function() {
@@ -120,4 +125,4 @@ $("document").ready(function() {
   });
 });
 
-renderTweets(data);a
+// renderTweets(data);
