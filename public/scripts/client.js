1fee73dc58db95dad46c85ebefb626a01d57bb88
Jhon Useche <jhon.useche@telus.com>
2022-12-06 09:36:30 -0700

    Add code to dinamically create and render the tweets from the data.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,88 @@
 /*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+* Client-side JS logic goes here
+* jQuery is already loaded
+* Reminder: Use (and do all your DOM work in) jQuery's document ready function
+*/
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  for (const index in tweets) {
+    const tweet = {
+      username: tweets[index].user.name,
+      avatar: tweets[index].user.avatars,
+      handle: tweets[index].user.handle,
+      text: tweets[index].content.text,
+      createdAt: tweets[index].created_at
+    };
+    console.log(tweet);
+    // calls createTweetElement for each tweet
+    const tweetElement = createTweetElement(tweet);
+    // takes return value and appends it to the tweets container
+    $("#tweets-container").append(tweetElement);
+  }
+};
+
+const createTweetElement = function(tweet) {
+  let $tweet = `
+  <article class="tweet">
+  <header>
+  <div class="left-box">
+  <img src="${tweet.avatar}">
+  <div class="tweet-name">
+  ${tweet.username}
+  </div>
+  </div>
+  <div class="handle">
+  ${tweet.handle}
+  </div>
+  </header>
+  <main>
+  <div class="tweet-text">
+  ${tweet.text}
+  </div>
+  <div class="tweet-divider"></div>
+  </main>
+  <footer>
+  <div>
+  ${tweet.createdAt}
+  </div>
+  <div class="tweet-options">
+  <i class="fa-solid fa-flag"></i>
+  <i class="fa-solid fa-retweet"></i>
+  <i class="fa-solid fa-heart"></i>
+  </div>
+        </footer>
+        </article>
+        `;/* Your code for creating the tweet element */
+  // ...
+  return $tweet;
+};
+
+// $("#tweets-container").ready(renderTweets(data));
+
+renderTweets(data);
