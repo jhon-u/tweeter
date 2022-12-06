f75577cdda3e88f7d5727609dfd3cb44acd6fa3b
Jhon Useche <jhon.useche@telus.com>
2022-12-06 13:57:32 -0700

    Add the timeago.js library to the index.html file and used the timeago.format function inside the for...loop in the renderTweets function to format the date.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,34 +4,10 @@
 * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
 $("document").ready(function() {
 
   const renderTweets = function(tweets) {
+    
     // loops through tweets
     for (const index in tweets) {
       const tweet = {
@@ -39,7 +15,7 @@ $("document").ready(function() {
         avatar: tweets[index].user.avatars,
         handle: tweets[index].user.handle,
         text: tweets[index].content.text,
-        createdAt: tweets[index].created_at
+        createdAt: timeago.format(tweets[index].created_at)
       };
       // calls createTweetElement for each tweet
       const tweetElement = createTweetElement(tweet);
