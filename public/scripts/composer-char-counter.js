f3efa07071eb6e5ef0394b2908f183be0929aaf8
Jhon Useche <jhon.useche@telus.com>
2022-12-07 12:20:42 -0700

    Create a variable for the $(".counter").

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,13 @@ $("document").ready(function() {
   let maxLength = 140;
   $("textarea").on("keyup", function() {
     let tweetLength = maxLength - $(this).val().length;
+    const counter = $(".counter");
     if (tweetLength < 0) {
-      $(".counter").addClass("counter-negative");
+      counter.addClass("counter-negative");
     }
     if (tweetLength >= 0) {
-      $(".counter").removeClass("counter-negative");
+      counter.removeClass("counter-negative");
     }
-    $(".counter").text(tweetLength);
+    counter.text(tweetLength);
   });
 });
